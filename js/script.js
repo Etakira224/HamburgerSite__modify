098e6e800c638d13af7bb12dd46a5e8160a7ee6c
kei.sakanishi@gmail.com <kei.sakanishi@gmail.com>
2022-02-22 20:41:46 +0900

    add files

--- js/script.js
+++ js/script.js
@@ -19,17 +19,32 @@ console.log(menu);
 console.log(closeBtn);
 
 btn.addEventListener('click', () => {
-    // menu.classList.add('is-open')
-    // console.log('OpenSidebar')
+    menu.classList.add('is-open')
+    // document.getElementById("Menu").style.transition('.5')
+    console.log('OpenSidebar')
     openSidebar();
     body.style.overflowY = "hidden";
-
 });
 
 closeBtn.addEventListener('click', () =>{
-    // menu.classList.remove('is-open')
-    // console.log('CloseSidebar')
+    menu.classList.remove('is-open')
+    menu.style.transition('none')
+    console.log('CloseSidebar')
     closeSidebar();
     body.style.overflowY = "visible";
 });
+
+/* お試し */
+// const mql = window.matchMedia('(min-width: 1026px)');
+
+// const handleMediaQuery = function(mql) {
+//     if (mql.matches) {
+//         $('js-menu').css('width', '100%');
+//     } else {
+//         $('js-menu').css('transition', '0');
+//     }
+// };
+
+// handleMediaQuery(mql);
+
 
